8c19435afaf836ddee0f0493faa016ced2c62a0b
sowmya <sowmya.kol@gmail.com>
2019-09-24 01:43:32 -0700

    commit

---
+++ pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
@@ -0,0 +1,64 @@
+package com.udacity.pricing.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * Represents the price of a given vehicle, including currency.
+ */
+@Entity
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String currency;
+    private BigDecimal price;
+
+
+    public Price() {
+    }
+
+    public Price(String currency, BigDecimal price) {
+        this.currency = currency;
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getVehicleId() {
+        return id;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.id = vehicleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "vehicleId=" + id +
+                ", currency='" + currency + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
